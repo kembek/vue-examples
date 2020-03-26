011d60c8c89bb872f33aeea0f35bf3bc51539f4b
Kembek <zevs.312.777@gmail.com>
2020-03-26 14:37:56 +0600

    login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <form class="form">
+      <div class="field">
+        <label class="field__label" for="username">Username:</label
+        ><input class="field__control" type="text" name="username" id="username" />
+      </div>
+      <div class="field">
+        <label class="field__label" for="password">Password:</label
+        ><input class="field__control" type="password" name="password" id="password" />
+      </div>
+      <button class="button">{{ 'Submit' | uppercase }}</button>
+    </form>
   </div>
 </template>
 
@@ -13,17 +22,75 @@ import HelloWorld from './components/HelloWorld.vue';
   components: {
     HelloWorld,
   },
+  filters: {
+    uppercase(text: string) {
+      console.log(this);
+      return text.toUpperCase();
+    },
+  },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private beforeCreate() {
+    console.log('before create', this);
+  }
+
+  private created() {
+    console.log('created', this);
+  }
+
+  private beforeMount() {
+    console.log('beforeMount', this);
+  }
+
+  private mounted() {
+    console.log('mounted', this);
+  }
+
+  private beforeUpdate() {
+    console.log('beforeUpdate', this);
+  }
+
+  private updated() {
+    console.log('updated', this);
+  }
+
+  private beforeDestroy() {
+    console.log('beforeDestroy', this);
+  }
+
+  private destroyed() {
+    console.log('destroyed', this);
+  }
+}
 </script>
 
 <style lang="scss">
+@import 'styles/variables';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5rem;
+}
+
+.form {
+  padding: 10px 5px;
+  // height: 600px;
+  border: 1px solid $form-title;
+}
+
+.field {
+  margin-bottom: $space_medium;
+  &__label {
+    display: inline-block;
+    width: 250px;
+    color: $control_text;
+    // background-color: $primary;
+    font-size: 2rem;
+    padding: 5px;
+  }
+
+  &__control {
+    color: $primary;
+    font-size: 2rem;
+  }
 }
 </style>
